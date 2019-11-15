969b4d636fce1509f35d6c6ff8d1f13730cc4308
tnvle <tnle@mum.edu>
2019-11-15 15:13:53 -0600

    update response type

---
+++ src/main/java/ea/sof/shared/models/TokenUser.java
@@ -0,0 +1,14 @@
+package ea.sof.shared.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenUser {
+    private String username;
+}
