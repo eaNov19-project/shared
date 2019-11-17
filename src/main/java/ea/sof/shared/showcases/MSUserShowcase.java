516bf824c5a0f57863b1fa288bccf344fa7c5421
urbanno1 <eduurbanus42@gmail.com>
2019-11-17 08:08:41 -0600

    updatig user model with additional constructor

--- src/main/java/ea/sof/shared/showcases/MSUserShowcase.java
+++ src/main/java/ea/sof/shared/showcases/MSUserShowcase.java
@@ -1,20 +1,11 @@
 package ea.sof.shared.showcases;
 
 import ea.sof.shared.models.Response;
-import ea.sof.shared.models.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 public interface MSUserShowcase {
-    @PostMapping("/add")
-    ResponseEntity<Response> addUser(@RequestBody User user);
-
-    @PostMapping("/edit")
-    ResponseEntity<Response> editUser(@RequestBody User user);
-
-    @PostMapping("/get/{email}")
+    @PostMapping("/api/users/get/{email}")
     ResponseEntity<Response> getUser(@PathVariable("email") String email);
-
 }
