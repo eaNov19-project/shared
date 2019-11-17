cc62e8317b7737b7380825d4461245eebfaac81c
urbanno1 <eduurbanus42@gmail.com>
2019-11-16 22:47:01 -0600

    updatig user model with additional constructor

--- src/main/java/ea/sof/shared/models/User.java
+++ src/main/java/ea/sof/shared/models/User.java
@@ -13,6 +13,7 @@ public class User {
     private String email;
     private String password;
     private String phone;
+    private String name;
 
     public User(String email, String password, String phone) {
         this.email = email;
