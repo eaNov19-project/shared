d636e3fed2f0d441e3944651ef5192b0ccd4168d
Ogabek Yuldashov <ogabek.yuldashov@gmail.com>
2019-11-18 17:02:43 -0600

    added SERVICE_SECRET to kafka config

--- src/main/java/ea/sof/shared/queue_models/AnswerQueueModel.java
+++ src/main/java/ea/sof/shared/queue_models/AnswerQueueModel.java
@@ -1,15 +1,11 @@
 package ea.sof.shared.queue_models;
 
 
-import ea.sof.shared.models.CommentAnswer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
