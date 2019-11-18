0f612d3b2d107a05013cfcea0b894350247dc3d4
Ogabek Yuldashov <ogabek.yuldashov@gmail.com>
2019-11-17 18:48:48 -0600

    Payloads added to KafkaMap

--- src/main/java/ea/sof/shared/entities/CommentAnswerEntity.java
+++ src/main/java/ea/sof/shared/entities/CommentAnswerEntity.java
@@ -1,13 +1,18 @@
 package ea.sof.shared.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Data
-//@Document(collection = "comments")
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class CommentAnswerEntity {
-//    @Id
     private String id;
     private String body;
     private LocalDateTime date;
