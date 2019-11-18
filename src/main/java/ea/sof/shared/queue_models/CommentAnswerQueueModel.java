3e427e478cf964c255de0e7d6f28799236c8108a
thanhtuyenle <ttle@mum.edu>
2019-11-17 22:31:52 -0600

    add CommentQuestion&AnswerQueueModel

---
+++ src/main/java/ea/sof/shared/queue_models/CommentAnswerQueueModel.java
@@ -0,0 +1,16 @@
+package ea.sof.shared.queue_models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentAnswerQueueModel {
+    private String id;
+    private String body;
+}
---
+++ src/main/java/ea/sof/shared/queue_models/CommentQuestionQueueModel.java
@@ -0,0 +1,16 @@
+package ea.sof.shared.queue_models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CommentQuestionQueueModel {
+    private String id;
+    private String body;
+
+}
