70e4641db46a0e5e52d6a7c0ccab4551785fca7b
Ogabek Yuldashov <ogabek.yuldashov@gmail.com>
2019-11-19 17:03:47 -0600

    AnswersShowcase endpoint changed

--- src/main/java/ea/sof/shared/showcases/MsAnswersShowcase.java
+++ src/main/java/ea/sof/shared/showcases/MsAnswersShowcase.java
@@ -1,19 +1,18 @@
 package ea.sof.shared.showcases;
 
+import ea.sof.shared.entities.AnswerEntity;
 import ea.sof.shared.models.Answer;
-import ea.sof.shared.models.QuestionFollowers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.List;
 
 public interface MsAnswersShowcase {
 
     // Retrieve answer by id
-    @GetMapping("/answers/{id}")
-    ResponseEntity<Answer> getAnswer(@PathVariable("id") String id);
+    @GetMapping("/answers/{id}/entity")
+    ResponseEntity<AnswerEntity> getAnswer(@PathVariable("id") String id);
 
     // Retrieve top 5 answers for given question
     @GetMapping("/answers/top5/{questionId}")
