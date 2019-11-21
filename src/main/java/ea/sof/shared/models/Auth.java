841b3c9691bf6a284e82195b1c1861ee8cee0bad
urbanno1 <eduurbanus42@gmail.com>
2019-11-20 18:20:15 -0600

    updatig user model with additional constructor

--- src/main/java/ea/sof/shared/models/Auth.java
+++ src/main/java/ea/sof/shared/models/Auth.java
@@ -10,14 +10,14 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Auth {
 
-    public Integer getUserId() {
+    public Long getUserId() {
         return userId;
     }
-    public void setUserId(Integer userId) {
+    public void setUserId(Long userId) {
         this.userId = userId;
     }
 
-    private Integer userId;
+    private Long userId;
     private String email;
     private String password;
 
